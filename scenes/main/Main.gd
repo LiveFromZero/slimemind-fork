f5e585a57572b960ec35859fce7a938c28dd62e4
Darleen <allerlei6@web.de>
2025-12-09 12:09:19 +0100

    refactor/ main now starts and stops the simulation

--- scenes/main/Main.gd
+++ scenes/main/Main.gd
@@ -1,1 +1,10 @@
 extends Node
+
+var is_running = false
+
+func _ready():
+	$Ui.simulation_toggled.connect(_on_ui_simulation_toggled)
+
+func _on_ui_simulation_toggled() -> void:
+	is_running = !is_running
+	get_tree().paused = !is_running
--- scenes/ui/UIController.gd
+++ scenes/ui/UIController.gd
@@ -1,1 +1,6 @@
 extends Node
+
+signal simulation_toggled
+
+func _on_StartPauseButton_pressed():
+	emit_signal("simulation_toggled")
