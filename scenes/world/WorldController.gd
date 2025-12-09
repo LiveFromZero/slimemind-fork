2b7d5cbb8d5abde33e1f814647dab816d14770a0
Darleen <allerlei6@web.de>
2025-12-09 12:43:56 +0100

    refactor/ world controller can place a segment

--- scenes/world/WorldController.gd
+++ scenes/world/WorldController.gd
@@ -1,1 +1,13 @@
 extends Node
+
+# Methode zum Spawnen eines Segments
+func spawn_segment() -> void:
+	# Lade die Szene als PackedScene
+	var packed: PackedScene = load("res://scenes/arms/ArmSegment.tscn") as PackedScene
+	
+	# Instanziiere die Szene
+	var instance: Node = packed.instantiate()
+	
+	# Füge sie im Baum hinzu
+	var parent_node: Node = get_node("Main/World/ArmRoot")
+	parent_node.add_child(instance)
