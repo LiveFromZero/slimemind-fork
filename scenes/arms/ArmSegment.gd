7131c18dc9020cadc97e952b751f29dba4771206
Darleen <allerlei6@web.de>
2025-12-17 12:17:15 +0100

    refactor/ working base

--- scenes/arms/ArmSegment.gd
+++ scenes/arms/ArmSegment.gd
@@ -1,1 +1,13 @@
-extends Node2D
+extends Node
+
+var ID
+var Vorgaenger
+var position_information = self.global_position
+
+signal color_changed(new_color: Color)
+
+var segment_color: Color = Color.RED
+
+func set_color(new_color: Color) -> void:
+	segment_color = new_color
+	color_changed.emit(new_color)
