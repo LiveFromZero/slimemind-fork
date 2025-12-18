9a7b6ea771ba6322fc684a76a24ce528eeb25e49
Darleen <allerlei6@web.de>
2025-12-18 11:01:56 +0100

    refactor/ Added start-arms depending on slider

--- scenes/ui/UIController.gd
+++ scenes/ui/UIController.gd
@@ -1,17 +1,14 @@
 extends Control  # oder Panel, je nach UI-Node
 
 signal simulation_toggled
-signal spawn_arm
+signal arms_count_changed(count: int)
 
 func _ready() -> void:
 	var start_btn = get_node("CanvasLayer2/Button") as Button
 	start_btn.pressed.connect(_on_start_button_pressed)
-	
-	var spawn_btn = get_node("CanvasLayer2/Button2") as Button
-	spawn_btn.pressed.connect(_spawn_button_pressed)
 
 func _on_start_button_pressed() -> void:
 	emit_signal("simulation_toggled")
 
-func _spawn_button_pressed() -> void:
-	emit_signal("spawn_arm")
+func _on_h_slider_value_changed(value: float) -> void:
+	arms_count_changed.emit(value)
--- scenes/world/WorldController.gd
+++ scenes/world/WorldController.gd
@@ -1,22 +1,41 @@
 extends Node
 
+@onready var arm_root := $"../ArmRoot"
+
+var arm_scene := load("res://scenes/arms/ArmSegment.tscn") as PackedScene
 var arm_segments = []
-
-# Methode zum Spawnen eines Segments
-func spawn_segment() -> void:
-	# Lade die Szene als PackedScene
-	var packed: PackedScene = load("res://scenes/arms/ArmSegment.tscn") as PackedScene
-	
-	# Instanziiere die Szene
-	var instance: Node = packed.instantiate()
-	
-	# Füge sie im Baum hinzu
-	var parent_node: Node = get_node("/root/Main/World/ArmRoot")
-	parent_node.add_child(instance)
-
-
-func _on_ui_spawn_arm() -> void:
-	spawn_segment()
 
 func _on_arm_root_arm_grew(arm: Node) -> void:
 	arm_segments.append(arm)
+
+
+func _spawn_arms(amount: int) -> void:
+	for i in amount:
+		var arm = arm_scene.instantiate()
+		arm_root.add_child(arm)
+
+func _remove_arms(amount: int) -> void:
+	for i in amount:
+		var arm = arm_root.get_child(-1)
+		arm.queue_free()
+
+func _reposition_arms() -> void:
+	var count := arm_root.get_child_count()
+	if count == 0:
+		return
+
+	var radius := 200.0
+	for i in count:
+		var angle := TAU * i / count
+		var arm := arm_root.get_child(i)
+		arm.position = Vector2(cos(angle), sin(angle)) * radius
+
+
+func _on_ui_arms_count_changed(count: int) -> void:
+	var current_count := arm_root.get_child_count()
+	if count > current_count:
+		_spawn_arms(count - current_count)
+		_reposition_arms()
+	elif count < current_count:
+		_remove_arms(current_count - count)
+		_reposition_arms()
