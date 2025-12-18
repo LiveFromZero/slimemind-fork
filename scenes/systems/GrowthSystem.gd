80d4f7158ffbf46efe76277e4069c228aad27d66
Darleen <allerlei6@web.de>
2025-12-18 12:16:28 +0100

    refactor/ added wobble for growing

--- scenes/systems/GrowthSystem.gd
+++ scenes/systems/GrowthSystem.gd
@@ -2,6 +2,7 @@ extends Node
 
 var segment_scene := load("res://scenes/arms/ArmSegment.tscn") as PackedScene
 var length: float = 30.0
+
 signal arm_grew(arm : Node2D)
 
 func _on_world_controller_grow_arm(arm_node: Node) -> void:
@@ -11,17 +12,19 @@ func spawn_segment_at_node(reference_node: Node2D) -> void:
 	# 1. Instanziere das Segment
 	var new_segment = segment_scene.instantiate() as Node2D
 
-	# 2. Berechne Richtung (Einheitsvektor nach oben, rotiert nach der Rotation des Referenz-Segments)
-	var direction = Vector2.UP.rotated(reference_node.global_rotation)
+	# 2. Berechne Richtung
+	var max_degrees_offset = 10.0  # maximale Abweichung
+	var random_offset = deg_to_rad(randf_range(-max_degrees_offset, max_degrees_offset))
+	var direction = Vector2.UP.rotated(reference_node.global_rotation + random_offset)
 
 	# 3. Positioniere das neue Segment am Ende des Referenz-Segments
 	new_segment.global_position = reference_node.global_position + direction * length
 
 	# 4. Rotation übernehmen
-	new_segment.rotation = reference_node.rotation  # lokale Rotation
+	new_segment.rotation = reference_node.rotation + random_offset  # lokale Rotation plus Zufall
 
-	# 5. Parent setzen (optional: gleiches Parent wie Referenz)
+	# 5. Parent setzen
 	reference_node.get_parent().add_child(new_segment)
-	
-	# 6. sending signal so this arm gets deleted from the array
+
+	# 6. Signal senden
 	arm_grew.emit(reference_node)
