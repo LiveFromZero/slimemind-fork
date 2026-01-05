60d6d19cec9f46a98fd64d7df8795d4ca79bec22
Darleen <allerlei6@web.de>
2026-01-05 15:21:40 +0100

    basic color changing now working

--- scenes/arms/ArmSegment.gd
+++ scenes/arms/ArmSegment.gd
@@ -4,13 +4,24 @@ var id
 var vorgaenger
 var position_information = self.global_position
 var life_points = 100
+var damage_per_second: float = 1.0  # 1 Punkt pro Sekunde
 
 signal color_changed(new_color: Color)
 signal segment_died
 
+func _process(delta: float):
+	life_points -= damage_per_second * delta
+	if life_points < 75:
+		set_color("green_yellow")
+	if life_points < 50:
+		set_color("olive-drab")
+	if life_points < 25:
+		set_color("olive")
+	if life_points < 1:
+		set_color("brown")
+		segment_died.emit()
+		queue_free()
+
+
 func set_color(new_color: Color) -> void:
 	color_changed.emit(new_color)
-
-func _on_tree_entered() -> void:
-	var birthTime = get_process_delta_time()
-	
--- scenes/arms/color_rect.gd
+++ scenes/arms/color_rect.gd
@@ -1,7 +1,7 @@
 extends ColorRect
 
 func _ready() -> void:
-	color = "Yellow"
+	color = "lawn_green"
 
 func _on_arm_segment_color_changed(new_color: Color) -> void:
 	color = new_color
