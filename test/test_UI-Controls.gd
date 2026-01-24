d1e67f3553a17d9d8049da3631778b478bcfd21c
Darleen <allerlei6@web.de>
2026-01-24 22:12:04 +0100

    ADD: #39 added test environment for UI

--- test/test_UI-Controls.gd
+++ test/test_UI-Controls.gd
@@ -1,22 +1,37 @@
 extends GdUnitTestSuite
 
 var runnerWorld: GdUnitSceneRunner
-var runnerArmSegment: GdUnitSceneRunner
+var StartButton : Button
 var FutteranzahlSlider : HSlider
 var FuttermengeSlider : HSlider
 var FeldgroesseSlider : HSlider
 var SpawnFoodButton : Button
 var LebenspunkteSlider : HSlider
+var SonnenlichtSlider : HSlider
+var TemperaturSlider : HSlider
+var LuftfeuchtigkeitSlider : HSlider
+var StartarmeSlider : HSlider
+var SimulationSpeedSlider : HSlider
+var ResetButton : Button
+var StatButton : Button
 
 func before_test() -> void:
 	runnerWorld = scene_runner("res://test/test_main.tscn")
+	StartButton = runnerWorld.find_child("Button")
 	FutteranzahlSlider = runnerWorld.find_child("FutteranzahlSlider")
 	FuttermengeSlider = runnerWorld.find_child("FuttermengeSlider")
 	FeldgroesseSlider = runnerWorld.find_child("FeldgrößeSlider")
 	SpawnFoodButton = runnerWorld.find_child("FoodSpawnButton")
 	LebenspunkteSlider = runnerWorld.find_child("LebenspunkteSlider")
+	SonnenlichtSlider = runnerWorld.find_child("SonnenlichtSlider")
+	TemperaturSlider = runnerWorld.find_child("TemperaturSlider")
+	LuftfeuchtigkeitSlider = runnerWorld.find_child("LuftfeuchtigkeitSlider")
+	StartarmeSlider = runnerWorld.find_child("StartarmeSlider")
+	SimulationSpeedSlider = runnerWorld.find_child("SimulationSpeedSlider")
+	ResetButton = runnerWorld.find_child("ResetButton")
+	StatButton = runnerWorld.find_child("Statistik")
 
-func test_UIFutterquellen(foodAmount:float, fieldSize:float,  test_parameters := [
+func test_UIFutterquellen(foodAmount:float, fieldSize:float, test_parameters := [
 	[FutteranzahlSlider.min_value, FeldgroesseSlider.min_value],
 	[FutteranzahlSlider.max_value, FeldgroesseSlider.min_value],
 	[FutteranzahlSlider.min_value, FeldgroesseSlider.max_value],
@@ -35,15 +50,16 @@ func test_UIFutterquellen(foodAmount:float, fieldSize:float,  test_parameters :=
 	#compare
 	assert_float(foodSpawned).is_equal(foodAmount)
 
+func test_UIFuttergroesse():
+	pass
 
 func test_StartButtonStartsSim():
 	# initiate
-	var startButton = runnerWorld.find_child("Button")
 	var segments = runnerWorld.find_child("ArmRoot")
 	var segmentsArray = segments.get_children()
 	
 	# run
-	startButton.pressed.emit()
+	StartButton.pressed.emit()
 	
 	#compare
 	assert_array(segmentsArray).is_not_empty()
