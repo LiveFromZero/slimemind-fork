68cdcd9567be78755e5949e47aa961c160557163
Darleen <allerlei6@web.de>
2026-01-24 22:01:11 +0100

    ADD: #39 added foodcount and fieldsize tests

--- test/test_UI-Controls.gd
+++ test/test_UI-Controls.gd
@@ -3,24 +3,33 @@ extends GdUnitTestSuite
 var runnerWorld: GdUnitSceneRunner
 var runnerArmSegment: GdUnitSceneRunner
 var FutteranzahlSlider : HSlider
+var FuttermengeSlider : HSlider
+var FeldgroesseSlider : HSlider
+var SpawnFoodButton : Button
+var LebenspunkteSlider : HSlider
 
 func before_test() -> void:
 	runnerWorld = scene_runner("res://test/test_main.tscn")
 	FutteranzahlSlider = runnerWorld.find_child("FutteranzahlSlider")
+	FuttermengeSlider = runnerWorld.find_child("FuttermengeSlider")
+	FeldgroesseSlider = runnerWorld.find_child("FeldgrößeSlider")
+	SpawnFoodButton = runnerWorld.find_child("FoodSpawnButton")
+	LebenspunkteSlider = runnerWorld.find_child("LebenspunkteSlider")
 
-func test_AnzahlFutterquellen(foodAmount:float, test_parameters := [
-	[FutteranzahlSlider.min_value],
-	[FutteranzahlSlider.max_value]
+func test_UIFutterquellen(foodAmount:float, fieldSize:float,  test_parameters := [
+	[FutteranzahlSlider.min_value, FeldgroesseSlider.min_value],
+	[FutteranzahlSlider.max_value, FeldgroesseSlider.min_value],
+	[FutteranzahlSlider.min_value, FeldgroesseSlider.max_value],
+	[FutteranzahlSlider.max_value, FeldgroesseSlider.max_value]
 ]):
 	# initiate
-	var slider : HSlider = runnerWorld.find_child("FutteranzahlSlider")
-	var spawnFoodButton : Button = runnerWorld.find_child("FoodSpawnButton")
 	var foodRoot : Node2D = runnerWorld.find_child("FoodManager")
 	var foodSpawned : float
 	
 	# run
-	slider.value_changed.emit(foodAmount)
-	spawnFoodButton.pressed.emit()
+	FutteranzahlSlider.value_changed.emit(foodAmount)
+	FeldgroesseSlider.value_changed.emit(fieldSize)
+	SpawnFoodButton.pressed.emit()
 	foodSpawned = foodRoot.get_child_count(true)
 	
 	#compare
