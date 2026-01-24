897f8a9ff143d3fbad4b1c051ddb26c42caee9b4
Darleen <allerlei6@web.de>
2026-01-24 12:30:07 +0100

    ADD: added time to statistic

--- scenes/ui/simulation_summary.gd
+++ scenes/ui/simulation_summary.gd
@@ -4,12 +4,10 @@ class_name SimulationSummary
 @onready var Dauer_label = $HFlowContainer/Dauer
 @onready var FutterGegessen_label = $HFlowContainer/FutterGegessen
 @onready var FuttermengeGegessen_label = $HFlowContainer/FuttermengeGegessen
-@onready var LängsterArm_label = $"HFlowContainer/LängsterArm"
 @onready var countDeadSegmentsLabel = $HFlowContainer/AnzahlToterSegmente
 
-func _on_world_statistik_send_data_to_summary(_time: String, _foodEaten: int, _foodAmountEaten: float, _longestArm: int, _countDeadSegments:int) -> void:
+func _on_world_statistik_send_data_to_summary(_time: String, _foodEaten: int, _foodAmountEaten: float, _countDeadSegments:int) -> void:
 	Dauer_label.text = "Dauer der Simulation: " + _time
 	FutterGegessen_label.text = "Anzahl an Futterquellen, die vollständig gefressen wurden: " + str(_foodEaten)
 	FuttermengeGegessen_label = "Futtermenge gegessen: " +  str(_foodAmountEaten)
-	LängsterArm_label.text = "längster Arm: " + str(_longestArm) # potenziell redundant
 	countDeadSegmentsLabel.text = "Anzahl toter Arme: " + str(_countDeadSegments)
--- scenes/world/worldstatistic.gd
+++ scenes/world/worldstatistic.gd
@@ -1,15 +1,14 @@
 extends Node2D
 class_name  WorldStatistic
 
-signal sendDataToSummary(_time:String, _foodEaten:int, _foodAmountEaten: float, _longestArm:int, _countDeadSegments)
+signal sendDataToSummary(_time:String, _foodEaten:int, _foodAmountEaten: float, _countDeadSegments:int)
 
 var time : String
 var foodEaten : int
 var foodAmountEaten : float
-var longestArm : int
 var countDeadSegments : int
-var startTimeSecond
-var endTimeSecond
+var startTimeSecond : int
+var endTimeSecond : int
 
 func _ready() -> void:
 	add_to_group("Statistik")
@@ -19,13 +18,12 @@ func set_defaults() -> void:
 	time = "0"
 	foodEaten = 0
 	foodAmountEaten = 0.0
-	longestArm = 0
 	countDeadSegments = 0
 
 func _on_ui_statistik_pressed() -> void:
 	endTimeSecond = Time.get_ticks_msec()
 	calculateTime()
-	sendDataToSummary.emit(time, foodEaten, foodAmountEaten, longestArm, countDeadSegments)
+	sendDataToSummary.emit(time, foodEaten, foodAmountEaten, countDeadSegments)
 
 func add_count_of_dead_segment() -> void:
 	countDeadSegments = countDeadSegments +1
@@ -34,11 +32,10 @@ func startTimer() -> void:
 	startTimeSecond = Time.get_ticks_msec()
 
 func calculateTime() -> void:
-	var total_seconds : float = endTimeSecond - startTimeSecond
-	if total_seconds < 0:
-		total_seconds = 0
+	var total_ms: int = max(0, endTimeSecond - startTimeSecond)
 
-	var minutes := int(total_seconds) / 60
-	var seconds := int(total_seconds) % 60
+	var total_seconds: int = total_ms / 1000
+	var minutes: int = total_seconds / 60
+	var seconds: int = total_seconds % 60
 
 	time = "%02d:%02d" % [minutes, seconds]
