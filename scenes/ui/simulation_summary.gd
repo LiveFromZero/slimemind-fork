3852f6b256fab89ecc6341292c8d2a23c9d0d209
Darleen <allerlei6@web.de>
2026-01-26 08:55:22 +0100

    ADD: improved ui naming

--- scenes/ui/simulation_summary.gd
+++ scenes/ui/simulation_summary.gd
@@ -7,7 +7,7 @@ class_name SimulationSummary
 @onready var countDeadSegmentsLabel = $HFlowContainer/AnzahlToterSegmente
 
 func _on_world_statistik_send_data_to_summary(_time: String, _foodEaten: int, _foodAmountEaten: int, _countDeadSegments:int) -> void:
-	Dauer_label.text = "Dauer der Simulation: " + _time
+	Dauer_label.text = "Zeit bis zum Tod des Pilzes: " + _time
 	FutterGegessen_label.text = "Anzahl an Futterquellen, die vollständig gefressen wurden: " + str(_foodEaten)
 	FuttermengeGegessen_label.text = "Futtermenge gegessen: " +  str(_foodAmountEaten)
-	countDeadSegmentsLabel.text = "Anzahl toter Arme: " + str(_countDeadSegments)
+	countDeadSegmentsLabel.text = "Anzahl an gewachsener und gestorbener Segmente: " + str(_countDeadSegments)
